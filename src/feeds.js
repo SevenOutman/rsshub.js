b2e098749650ec36f0b45ea4ff35ea3cb1ed4a6e
hyrious <hyrious@outlook.com>
2018-10-10 18:04:28 +0800

    update 124 routes & add support for optional params (#2)
    * add support for optional params
    
    * update many feeds
    
    * add update tool
    
    * add usage of the update-tool
    
    * fix slugs in feeds/*.json
    
    * fix interface RSSHubFeeds
    
    * fix no param condition
    
    * fix '3dm' in interface RSSHubFeeds
    
    * fix telegram.json

--- src/feeds.js
+++ src/feeds.js
@@ -1,31 +1,31 @@
 const request = require('./request');
-const r = require.context('../feeds', false, /\.json$/)
+const r = require.context('../feeds', false, /\.json$/);
 
-function dashToUnderscore(str) {
-    return str.replace(/[^a-z0-9]/g, '_');
-}
+const dashToUnderscore = str => str.replace(/[^a-z0-9]/g, '_');
 
-function fillURL(url, params = {}) {
-    return url.replace(/:\w+(?=\/|$)/g, function (match) {
-        return params[match.substr(1, match.length - 1)] || match;
-    });
-}
-
-function createRequestFunction(url) {
-    return function (params) {
-        return request(fillURL(url, params));
+const fillURL = (url, params = {}) => {
+    let [path, ...patterns] = url.split('/:');
+    for (let pattern of patterns) {
+        const isOptional = pattern.endsWith('?');
+        if (isOptional) pattern = pattern.slice(0, -1);
+        if (pattern in params) {
+            path += '/' + params[pattern];
+        } else if (!isOptional)
+            throw new Error(`[${pattern}] is required in [${url}]`);
     }
-}
+    return path;
+};
 
-const feeds = r.keys().reduce(function (acc, key) {
+const createRequestFunction = url => params => request(fillURL(url, params));
+
+const feeds = r.keys().reduce((acc, key) => {
     const name = dashToUnderscore(key.match(/^\.\/(.+?)\.json$/)[1]);
     const { namespace, paths = {}, path } = r(key);
     return Object.assign(acc, {
-        [name]: Object.keys(paths).reduce(function (result, path) {
-            return Object.assign(result, {
-                [dashToUnderscore(path)]: createRequestFunction(`/${namespace}/${paths[path]}`)
-            });
-        }, path !== undefined ? createRequestFunction(`/${path}`) : {})
+        [name]: Object.keys(paths).reduce((result, path) => {
+            const fn = createRequestFunction(`/${namespace}/${paths[path]}`);
+            return Object.assign(result, { [dashToUnderscore(path)]: fn });
+        }, path != null ? createRequestFunction(`/${namespace}/${path}`) : {}),
     });
 }, {});
 
