078938d5dcbaf0bd8511d7af87d13c44d69ba67a
hyrious <hyrious@outlook.com>
2018-10-10 16:51:59 +0800

    add update tool

---
+++ tools/update.js
@@ -0,0 +1,98 @@
+const { isEmpty, first, size, capitalize } = require('lodash');
+const { ensureDirSync, outputFile, outputJson, readJson } = require('fs-extra');
+
+const got = require('got').extend({
+  baseUrl: 'http://localhost:1200',
+  json: true,
+});
+
+const clean = ([path, params]) => {
+  const result = [path.join('/'), ...params].join('/:');
+  return result.replace(/^\/|\/$/g, '');
+};
+
+(async () => {
+  const { body: { status, data, message } } = await got('/api/routes');
+  console.log(message);
+  if (status !== 'success') return;
+  console.log('start processing routes ...');
+  
+  const feeds = {};
+  for (const [name, { routes }] of Object.entries(data)) {
+    feeds[name] = { namespace: name };
+    const paths = routes.map(r => {
+      const [path, ...params] = r.split('/:');
+      return [path.split('/').slice(2), params];
+    });
+    feeds[name].paths = {};
+    for (let [path, params] of paths) {
+      feeds[name].paths[path.join('_')] = clean([path, params]);
+    }
+    if (feeds[name].paths['']) {
+      feeds[name].path = feeds[name].paths[''];
+      delete feeds[name].paths[''];
+      if (isEmpty(feeds[name].paths))
+        delete feeds[name].paths;
+    }
+  }
+  const descs = (await readJson('descriptions.json', { throws: false })) || {};
+  for (const [name, desc] of Object.entries(descs))
+    if (feeds[name]) feeds[name].description = desc;
+
+  ensureDirSync('feeds');
+  for (const [name, data] of Object.entries(feeds)) {
+    outputJson(`feeds/${name}.json`, data, { spaces: 2 });
+  }
+
+  let d = '';
+  for (let [name, { paths, path }] of Object.entries(feeds)) {
+    let x = '';
+    if (!isNaN(name[0])) name = 'I' + name;
+    name = name.replace(/\.\w/g, x => x[1].toUpperCase());
+    const interfaceName = capitalize(name) + 'Feed';
+    let e = '';
+    if (path != null) {
+      const params = path.split(/\/?:/).slice(1).map(p => {
+        let t = `${p}: string`;
+        if (p.includes('id') || p.includes('num')) t += ' | number';
+        return t;
+      });
+      const f = isEmpty(params) ? '' : `<{ ${params.join(', ')} }>`;
+      e = ` extends RSSHubFeedEndpoint${f}`;
+    }
+    const c = [];
+    if (paths != null) {
+      for (let [slug, url] of Object.entries(paths)) {
+        const params = url.split(/\/?:/).slice(1).map(p => {
+          let t = `${p}: string`;
+          if (p.includes('id') || p.includes('num')) t += ' | number';
+          return t;
+        });
+        const key = `    ${slug.replace('-', '_')}`;
+        const f = isEmpty(params) ? '' : `<{ ${params.join(', ')} }>`;
+        c.push(`${key}: RSSHubFeedEndpoint${f};`);
+      }
+    }
+    x += `interface ${interfaceName}${e} {\n${c.join('\n')}\n}\n`;
+    d += x + '\n';
+  }
+  d = `import {RSSHubRequestSetup} from "./index";
+
+interface RSSHubFeedEndpoint<T = void> {
+    (params?: T): RSSHubRequestSetup;
+}
+
+interface Feed {
+    [key: string]: RSSHubFeedEndpoint<any>;
+}
+` + d + `declare const Feeds: RSSHubFeeds;
+export default Feeds;
+`;
+  outputFile('feeds.d.ts', d);
+  console.log('done.');
+
+  // const hub = require('rsshub.js');
+  // hub.config({ baseURL: 'http://localhost:1200' });
+  // const json = await hub.feeds['3dm'].news().json();
+  // console.log('test requesting 3dm.news() =>', size(json));
+})();
